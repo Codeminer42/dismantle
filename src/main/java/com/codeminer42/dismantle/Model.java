21dead3a67ecf61229b3140e6aa144c7b6c8330b
Eduardo Gurgel <edgurgel@gmail.com>
2013-02-05 22:23:10 -0300

    Fix unnecessary instance method

--- src/main/java/com/codeminer42/dismantle/Model.java
+++ src/main/java/com/codeminer42/dismantle/Model.java
@@ -147,7 +147,7 @@ public abstract class Model {
         return mirrorOnThis.get().field(property);
     }
 
-    private String capitalize(String word) {
+    private static String capitalize(String word) {
         return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
